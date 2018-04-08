2d67bc284b36f6dd5400dfaea7a46cde4b5df36c
Lucas362 <lucasvitorifg@gmail.com>
2018-04-08 13:15:34 -0300

    added heap sort

--- sort.c
+++ sort.c
@@ -26,6 +26,45 @@ void showVector(int vector[], int size){
     puts("====Fim Vetor");
 }
 
+void heapSort(int a[], int n) {
+   int i = n / 2, pai, filho, t;
+   int v2[n];
+   /* Cópia do vetor para não interferir com a outra ordenação */
+   for(i = 0; i < n; i++){
+       v2[i] = a[i];
+   }
+   clock_t ti = clock();
+   while(1) {
+      if (i > 0) {
+          i--;
+          t = v2[i];
+      } else {
+          n--;
+          if (n == 0){
+              printf("teste\n");
+              printf("====Tempo gasto %lfs====\n\n", (double)(clock()-ti)/CLOCKS_PER_SEC);
+              return;
+          }
+          t = v2[n];
+          v2[n] = v2[0];
+      }
+      pai = i;
+      filho = i * 2 + 1;
+      while (filho < n) {
+          if ((filho + 1 < n)  &&  (v2[filho + 1] > v2[filho]))
+              filho++;
+          if (v2[filho] > t) {
+             v2[pai] = v2[filho];
+             pai = filho;
+             filho = pai * 2 + 1;
+          } else {
+             break;
+          }
+      }
+      v2[pai] = t;
+   }
+}
+
 void shellSort(int vector[], int size){
     int gap = size/2;
     int i, atual, j, printar;
@@ -79,8 +118,9 @@ void insertionSort(int vector[], int size){
 void menu(){
     puts("1 - Gerar novo vetor");
     puts("2 - Fazer sort (Shell Sort)");
-    puts("3 - Fazer sort(Insertion Sort)");
-    puts("4 - Printar vetor desordenado");
+    puts("3 - Fazer sort (Shell Sort)");
+    puts("4 - Fazer sort(Insertion Sort)");
+    puts("5 - Printar vetor desordenado");
     puts("0 - Sair");
 }
 
@@ -98,14 +138,18 @@ int main(){
                 createVector(vector, size);
                 break;
             case 2:
-                size>0 ? shellSort(vector, size) :
+                size>0 ? heapSort(vector, size) :
                  printf("Vetor nao inicializado\n");
                 break;
             case 3:
-                size>0 ? insertionSort(vector, size) :
+                size>0 ? shellSort(vector, size) :
                  printf("Vetor nao inicializado\n");
                 break;
             case 4:
+                size>0 ? insertionSort(vector, size) :
+                 printf("Vetor nao inicializado\n");
+                break;
+            case 5:
                 size>0 ? showVector(vector, size) :
                  printf("Vetor nao inicializado\n");
         }
